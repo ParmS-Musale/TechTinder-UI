e390e629ee256bbfbc2358de2d7d6d4401fa8e09
ParmS-Musale <musaleparm9541@gmail.com>
2025-01-07 18:03:08 +0530

    Refactor component structure: move components to a dedicated directory, enhance user authentication flow, and improve error handling in Login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,13 +1,15 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import axios from "axios";
-import { addUser } from "./utils/userSlice";
-import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import { BASE_URL } from "./utils/constants";
+import { addUser } from "../utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
 
 const Login = () => {
   const [emailId, setEmailId] = useState("hitler@gmail.com");
   const [password, setPassword] = useState("hitler@123");
+  const [error, setError] = useState("");
+
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -15,58 +17,57 @@ const Login = () => {
     try {
       const res = await axios.post(
         BASE_URL + "/login",
-        { emailId, password },
+        {
+          emailId,
+          password,
+        },
         { withCredentials: true }
       );
-      console.log(res.data);
       dispatch(addUser(res.data));
-      return navigate("/")
-
+      return navigate("/");
     } catch (err) {
-      console.error(err);
+      setError(err?.response?.data || "Something went wrong");
     }
   };
 
   return (
-    <div className="flex justify-center my-12 ">
-      <div className="card bg-zinc-900 text-black w-96 shadow-xl ">
-        <div className="card-body ">
+    <div className="flex justify-center my-10">
+      <div className="card bg-base-300 w-96 shadow-xl">
+        <div className="card-body">
           <h2 className="card-title justify-center">Login</h2>
           <div>
-            <label className="form-control w-full max-w-xs">
+            <label className="form-control w-full max-w-xs my-2">
               <div className="label">
-                <span className="label-text text-black">Email ID</span>
+                <span className="label-text">Email ID:</span>
               </div>
               <input
-                type="email"
-                placeholder=""
-                className="input input-bordered w-full max-w-xs"
+                type="text"
                 value={emailId}
+                className="input input-bordered w-full max-w-xs"
                 onChange={(e) => setEmailId(e.target.value)}
               />
-              <div className="label"></div>
             </label>
-            <label className="form-control w-full max-w-xs">
+            <label className="form-control w-full max-w-xs my-2">
               <div className="label">
-                <span className="label-text text-white">Password</span>
+                <span className="label-text">Password</span>
               </div>
               <input
-                type="email"
-                placeholder=""
-                className="input input-bordered w-full max-w-xs"
+                type="text"
                 value={password}
+                className="input input-bordered w-full max-w-xs"
                 onChange={(e) => setPassword(e.target.value)}
               />
-              <div className="label"></div>
             </label>
           </div>
-          <div className="card-actions justify-center">
-            <button className="btn bg-slate-950 text-white " onClick={handleLogin}>Login</button>
+          <p className="text-red-600">{error}</p>
+          <div className="card-actions justify-center m-2">
+            <button className="btn btn-primary" onClick={handleLogin}>
+              Login
+            </button>
           </div>
         </div>
       </div>
     </div>
   );
 };
-
 export default Login;
